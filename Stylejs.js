eb422e13a0ce61f7e7e5fd7ae31dc8970bdf42e6
Mahlatse <79442138+Mahlatse007@users.noreply.github.com>
2024-03-04 15:21:57 +0200

    Update!!!

--- Stylejs.js
+++ Stylejs.js
@@ -13,30 +13,3 @@ function scrollToDiv(className) {
 function closeWindow(){
   window.close();
 }
-
-
-document.getElementById('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    var formData = new FormData(this);
-    var object = {};
-
-    formData.forEach(function(value, key){
-        object[key] = value;
-    });
-
-    var json = JSON.stringify(object);
-
-    console.log(json);
-});
-
-fetch('mahlatsemakgati72@gmail.com', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: json,
-})
-.then((response) => response.json())
-.then((data) => console.log('Email sent: ', data))
-.catch((error) => console.error('Error:', error));
