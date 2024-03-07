01883764b83bd320d073780f19aed74493d82f03
Mahlatse <79442138+Mahlatse007@users.noreply.github.com>
2024-03-07 14:39:49 +0200

    Updated CV Added projects and links

--- Stylejs.js
+++ Stylejs.js
@@ -13,3 +13,36 @@ function scrollToDiv(className) {
 function closeWindow(){
   window.close();
 }
+
+//Testing Contact me
+function validateForm() {
+    var email = document.getElementById("email").value;
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var message = document.getElementById("textarea").value;
+
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (email.trim() === "" || !emailRegex.test(email)) {
+        alert("Please enter a valid email address.");
+        return false;
+    }
+
+    if (firstName.trim() === "") {
+        alert("Please enter your first name.");
+        return false;
+    }
+
+    if (lastName.trim() === "") {
+        alert("Please enter your last name.");
+        return false;
+    }
+
+    if (message.trim() === "") {
+        alert("Please enter a message.");
+        return false;
+    }
+
+    alert("Thank. I will get back to you");
+    return true;
+}
